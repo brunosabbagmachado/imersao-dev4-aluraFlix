e46d61e5da2726547cf3fd9979940ea72cafe8e1
Bruno Machado <bruno.sabbagmachado@outlook.com>
2022-03-17 10:34:11 -0300

    finalizando aula 4, iniciando aula 5

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var listaFilmes = [
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
-  "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-  "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
-];
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
 
-for (var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src=" + listaFilmes[i] + ">");
+  if (filmeFavorito.endsWith(".jpg")) {
+      listarFilmesNaTela();    
+  } else {
+      console.error("Endereço do filme inválido")
+  }
+
+  document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filmeFavorito) {
+    var elementoFilmeFavotiro = "<img src=" + filmeFavorito + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavotiro;
 }
