5faaf538cfd740846820d97c39a247163ec43372
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-09-11 22:35:14 -0300

    atualizando js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 function adicionarFilme() {
-  var filmeFavorito = document.getElementById("filme").value;
+    var filmeFavorito = document.getElementById("filme").value;
 
-  if (filmeFavorito.endsWith(".jpg")) {
-      listarFilmesNaTela(filmeFavorito);    
-  } else {
-      console.error("Endereço do filme inválido")
-  }
-
-  document.getElementById("filme").value = "";
+    if (filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito);
+    } else {
+        console.error("Endereço do filme inválido")
+    }
+    document.getElementById("filme").value = "";
 }
 
 function listarFilmesNaTela(filme) {
-    var elementoFilmeFavotiro = "<img src=" + filme + ">";
+    var elementoFilmeFavotiro = `<img src= ${filme}>`;
     var elementoListaFilmes = document.getElementById("listaFilmes");
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavotiro;
 }
